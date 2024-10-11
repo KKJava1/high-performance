c9ebac82a84dba8d599a47c1dc09adc64f9bb4ed
k <824135348@qq.com>
2024-10-12 00:47:27 +0800

    Lock基础实现

--- src/main/java/com/mashibing/juc/c_021_02_AQS/MLock.java
+++ src/main/java/com/mashibing/juc/c_021_02_AQS/MLock.java
@@ -4,22 +4,50 @@
  */
 package com.mashibing.juc.c_021_02_AQS;
 
+import java.util.Iterator;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.LockSupport;
 
+//实现独享锁
 public class MLock implements Lock {
 
-    private Sync sync = new Sync();
+//    private Sync sync = new Sync();
 
+    volatile AtomicReference<Thread> owner = new AtomicReference<>();
+
+    //保存等待的线程
+    volatile LinkedBlockingDeque<Thread> waiters = new LinkedBlockingDeque<>();
     @Override
     public void lock() {
-        sync.acquire(1);
+        boolean addQ = true;
+        if(!tryLock()){
+            if(addQ){
+                //等待
+                waiters.offer(Thread.currentThread());
+                //挂起当前的线程
+               addQ = false;
+            }else {
+                LockSupport.park();
+            }
+
+        }
+        waiters.remove(Thread.currentThread());
     }
 
     @Override
     public void unlock() {
-        sync.release(1);
+//        sync.release(1);
+        if(owner.compareAndSet(Thread.currentThread(),null)){
+            Iterator<Thread> iterator = waiters.iterator();
+            while (iterator.hasNext()){
+                Thread next = iterator.next();
+                LockSupport.unpark(next);
+            }
+        }
     }
 
     @Override
@@ -29,7 +57,7 @@ public class MLock implements Lock {
 
     @Override
     public boolean tryLock() {
-        return false;
+        return owner.compareAndSet(null,Thread.currentThread());
     }
 
     @Override
